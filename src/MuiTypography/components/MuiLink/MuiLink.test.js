b605870ec0ece38050f4423357904c227edfd90e
Alex James <apjames93@gmail.com>
2020-03-22 20:40:17 -0600

    MuiLink to use in Typography not sure if there is alot of use for it but nice to have the underline text to look like a hyperlink

---
+++ src/MuiTypography/components/MuiLink/MuiLink.test.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import { MemoryRouter } from 'react-router-dom';
+import renderer from 'react-test-renderer';
+import MuiLink from './MuiLink';
+
+function setup() {
+  const props = {
+    herf: 'herf.com',
+    text: 'text',
+  };
+  const comp = shallow(<MuiLink {...props} />);
+  return { comp, props };
+}
+
+describe('<MuiLink />', () => {
+  it('renders MuiLink', () => {
+    const { comp } = setup();
+    expect(comp).toBeDefined();
+  });
+
+  it('renders MuiLink', () => {
+    const { replace } = window.location;
+    window.location.replace = jest.fn();
+    const { comp } = setup();
+    comp.find('WithStyles(ForwardRef(Link))').first().simulate('click');
+    expect(window.location.replace).toBeCalled();
+    window.location.replace = replace;
+  });
+
+
+  test('snapshot', () => {
+    const { props } = setup();
+    const tree = renderer.create((
+      <MemoryRouter>
+        <MuiLink {...props} />
+      </MemoryRouter>
+    ));
+    expect(tree).toMatchSnapshot();
+  });
+});
