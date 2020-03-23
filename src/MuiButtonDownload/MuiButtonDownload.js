e1408f208216f5d7344a2561b2dbf942c3ed9c01
Alex James <apjames93@gmail.com>
2020-03-23 13:15:39 -0600

    pulled in MuiButton

--- src/MuiButtonDownload/MuiButtonDownload.js
+++ src/MuiButtonDownload/MuiButtonDownload.js
@@ -1,10 +1,8 @@
-import React, { lazy, Suspense } from 'react';
+import React, { createElement, lazy, Suspense } from 'react';
 import PropTypes from 'prop-types';
-import Storyblok from '../utils/Storyblok';
-import Button from '@material-ui/core/Button';
 import downloadUrl from '../utils/downloadUrl';
 
-const MuiIcon = lazy(() => import('../MuiIcon/MuiIcon'));
+const MuiButton = lazy(() => import('../MuiButton/MuiButton'));
 
 /**
  * MuiButtonDownload onClick bug is opening url in a new tab in the browser instead of dowloading.
@@ -12,74 +10,46 @@ const MuiIcon = lazy(() => import('../MuiIcon/MuiIcon'));
  */
 
 export const MuiButtonDownload = ({
-  buttonText,
-  variant,
-  color,
-  size,
-  rootClass,
-  startIcon,
-  endIcon,
+  button,
   herf,
   fileName,
 }) => {
-  const styles = Storyblok.arrayToMuiStyles(rootClass);
+  const components = {
+    MuiButton,
+  };
 
   const onClick = async () => {
     downloadUrl(herf, fileName);
   };
 
+  const muibutton = button[0];
+
   return (
-    <Button
-      className={styles.root}
-      variant={variant}
-      color={color}
-      size={size}
-      onClick={onClick}
-      startIcon={startIcon ? (
-        <Suspense fallback={<div />}>
-          <MuiIcon iconName={startIcon} />
-        </Suspense>
-      ) : null}
-      endIcon={endIcon ? (
-        <Suspense fallback={<div />}>
-          <MuiIcon iconName={endIcon} />
-        </Suspense>
-      ) : null}
-    >
-      {buttonText}
-    </Button>
+    <Suspense fallback={<div />}>
+      {
+      muibutton
+        ? createElement(
+          components[muibutton.component],
+          { ...muibutton, onClick },
+        )
+        : null
+    }
+    </Suspense>
   );
 };
 
 export default MuiButtonDownload;
 
 MuiButtonDownload.propTypes = {
-  /** mui prop: 'default'| 'inherit'| 'primary'| 'secondary' */
-  color: PropTypes.string,
-  /** mui prop: 'small'| 'medium'| 'large' */
-  size: PropTypes.string,
-  /** mui prop: 'text' |'outlined'| 'contained */
-  variant: PropTypes.string,
-  /** name of Icon  */
-  startIcon: PropTypes.string,
-  /** name of Icon */
-  endIcon: PropTypes.string,
-  /** stroyblok multiselect of css classes */
-  rootClass: PropTypes.arrayOf(PropTypes.string),
+  /** MuiButton Allowed maximum: 1 */
+  button: PropTypes.arrayOf(PropTypes.shape({
+    component: PropTypes.string.isRequired,
+  })).isRequired,
 
-  /** button text */
-  buttonText: PropTypes.string.isRequired,
   /** url of the file you want to be downloaded */
   herf: PropTypes.string.isRequired,
   /** name of file */
   fileName: PropTypes.string.isRequired,
 };
 
-MuiButtonDownload.defaultProps = {
-  startIcon: null,
-  endIcon: null,
-  rootClass: [],
-  variant: 'text',
-  color: 'secondary',
-  size: 'medium',
-};
+MuiButtonDownload.defaultProps = {};
