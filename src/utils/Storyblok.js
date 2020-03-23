c23406b185e0bfe7451925f75f66509ef1d750a8
Alex James <apjames93@gmail.com>
2020-03-22 20:17:01 -0600

    might needs some work but checkForMetaTag will look to see if we have metatags for facebook linkdin and twitter and if not set them to storyblok seo

--- src/utils/Storyblok.js
+++ src/utils/Storyblok.js
@@ -21,12 +21,67 @@ const client = new StoryblokClient({
   },
 });
 
+const checkForMetaTag = (id, property = null, name = null) => {
+  let element = document.getElementById(id);
+  if (!element) {
+    element = document.createElement('meta');
+    if (property) element.setAttribute('property', property);
+    if (name) element.setAttribute('name', property);
+  }
+  return element;
+};
+
+
+const setMetadata = (res) => {
+  if (res.data.story.content.seo) {
+
+    const metadataTag = checkForMetaTag('metadataTitle');
+    const metaOgTitle = checkForMetaTag('metaOgTitle', 'og:title');
+    const metaOgImage = checkForMetaTag('metaOgImage', 'og:image');
+    const metaOgDescription = checkForMetaTag('metaOgDescription', 'og:description');
+    const metaOgUrl = checkForMetaTag('metaOgUrl', 'og:url');
+
+    const metaTwitterTitle = checkForMetaTag('metaTwitterTitle', null, 'twitter:title');
+    const metaTwitterDescription = checkForMetaTag('metaTwitterDescription', null, 'twitter:description');
+    const metaTwitterImage = checkForMetaTag('metaTwitterImage', null, 'twitter:image');
+
+    if (res.data.story.content.seo.title) {
+      const {
+        title,
+        og_image,
+        og_title,
+        description,
+        twitter_image,
+        twitter_title,
+        og_description,
+        og_url,
+        twitter_description,
+      } = res.data.story.content.seo;
+      metadataTag.setAttribute('content', description);
+      metadataTag.setAttribute('title', title);
+      document.title = title;
+
+      // setting meta tags for facebook and linkedin
+      metaOgTitle.setAttribute('content', og_title);
+      metaOgImage.setAttribute('content', og_image);
+      metaOgDescription.setAttribute('content', og_description);
+      metaOgUrl.setAttribute('content', (og_url || ''));
+
+      // setting meta tags for twitter
+      metaTwitterTitle.setAttribute('content', twitter_title);
+      metaTwitterDescription.setAttribute('content', twitter_description);
+      metaTwitterImage.setAttribute('content', twitter_image);
+    }
+  }
+};
+
 const version = process.env.REACT_APP_ENV === 'production' ? 'published' : 'draft';
 
 class StoryBlock {
   static async get(route, options = {}) {
     try {
       const res = await client.get(`cdn/stories/${route}`, Object.assign({ version }, options));
+      setMetadata(res);
       return res.data.story.content.body;
     } catch (err) {
       throw new Error(err);
