9f89a7f1db733bd4134d31787ee57fec20702daf
Alex James <apjames93@gmail.com>
2020-03-28 20:14:14 -0600

    MuiStoryblok is the entry point for the app

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,3 +1,38 @@
-import MuiStoryblok from './components/MuiStoryblok';
+import React from 'react';
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+import PropTypes from 'prop-types';
+import StoryBlokPage from './components/StoryBlokPage/StoryBlokPage';
+import './styles/styles.scss';
+
+export const MuiStoryblok = ({
+  theme,
+  accessToken,
+  version,
+}) => {
+  const muiTheme = createMuiTheme(theme);
+  return (
+    <MuiThemeProvider theme={muiTheme}>
+      <StoryBlokPage
+        accessToken={accessToken}
+        version={version}
+      />
+    </MuiThemeProvider>
+  );
+};
 
 export default MuiStoryblok;
+
+MuiStoryblok.propTypes = {
+  /** theme for mui */
+  theme: PropTypes.shape(),
+  /** storyblok prop  'published' : 'draft' */
+  version: PropTypes.string,
+
+  /** acess key from storyblok you can make them in storyblok settings */
+  accessToken: PropTypes.string.isRequired,
+};
+
+MuiStoryblok.defaultProps = {
+  theme: {},
+  version: 'draft',
+};
