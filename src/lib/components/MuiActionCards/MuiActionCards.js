122a1babea93d6785d166951beb420013bc4c856
Isaac Sunoo <isaacsunoo@gmail.com>
2020-05-29 11:20:52 -0600

    started adding action Cards

---
+++ src/lib/components/MuiActionCards/MuiActionCards.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export const MuiActionCards = ({
+  styles,
+  actionCards,
+  icon,
+  cardTitle,
+  cardBody,
+}) => {
+
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default MuiActionCards;
+
+MuiActionCards.propTypes = {
+
+};
+
+MuiActionCards.defaultProps = {
+
+};
