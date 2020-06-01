098faa68a88e8290cd0c129770560f9e5ea5654e
Isaac Sunoo <isaacsunoo@gmail.com>
2020-06-01 11:45:03 -0600

    added expandable menu to drawers

---
+++ src/lib/components/MuiIconButtonDrawer/components/MuiListDropdown/MuiListDropdown.js
@@ -0,0 +1,57 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import { List, Collapse } from '@material-ui/core';
+import MuiListExpansion from '../MuiListExpansion/MuiListExpansion';
+import MuiList from '../../../MuiList/MuiList';
+import StoryBlok from '../../../../utils/Storyblok';
+
+
+export const MuiListDropdown = ({
+  rootClass,
+  listExpansion,
+  content,
+}) => {
+  const styles = StoryBlok.arrayToMuiStyles(rootClass);
+  const [open, setOpen] = useState(false);
+  const handleClick = () => {
+    setOpen(!open);
+  };
+
+  return (
+    <List className={styles.root}>
+      <MuiListExpansion {...listExpansion[0]} handleClick={handleClick} open={open} />
+      <Collapse in={open} timeout="auto" unmountOnExit>
+        {content.map((item, index) => (<MuiList {...item} key={index} />))}
+      </Collapse>
+    </List>
+  );
+
+};
+
+export default MuiListDropdown;
+
+MuiListDropdown.propTypes = {
+  /**
+   * storyblok multiselect of css classes
+   * Override or extend the styles applied to the component
+   * */
+  rootClass: PropTypes.arrayOf(PropTypes.string),
+  /**
+   * components:
+   * MuiListItem
+   */
+  content: PropTypes.arrayOf(PropTypes.shape({
+    component: PropTypes.string.isRequired,
+  })).isRequired,
+  /**
+   * components:
+   * MuiListItem
+   */
+  listExpansion: PropTypes.arrayOf(PropTypes.shape({
+    component: PropTypes.string.isRequired,
+  })).isRequired,
+};
+
+MuiListDropdown.defaultProps = {
+  rootClass: [],
+};
---
+++ src/lib/components/MuiIconButtonDrawer/components/MuiListDropdown/MuiListDropdown.test.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import { MemoryRouter } from 'react-router-dom';
+import renderer from 'react-test-renderer';
+import MuiListDropdown from './MuiListDropdown';
+
+function setup() {
+  const props = {
+    listExpansion: [{
+      component: 'MuiListExpansion',
+      icon: [{
+        component: 'MuiListItemIcon',
+        content: [{
+          component: 'MuiIcon',
+          iconName: 'star',
+        }],
+      }],
+      primaryText: 'list Text',
+    }],
+    content: [{
+      component: 'MuiList',
+      content: [{
+        component: 'MuiListItem',
+      }],
+    }],
+  };
+  const comp = shallow(<MuiListDropdown {...props} />);
+  return { comp, props };
+}
+
+describe('<MuiListDropdown />', () => {
+  it('renders MuiListDropdown', () => {
+    const { comp } = setup();
+    expect(comp).toBeDefined();
+  });
+
+  test('snapshot', () => {
+    const { props } = setup();
+    const tree = renderer.create((
+      <MemoryRouter>
+        <MuiListDropdown {...props} />
+      </MemoryRouter>
+    ));
+    expect(tree).toMatchSnapshot();
+  });
+});
