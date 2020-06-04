c17bac53f346851f4b0988859771a0f91a47a758
Isaac Sunoo <isaacsunoo@gmail.com>
2020-06-04 11:30:51 -0600

    changed action card container tests to mount

--- src/lib/components/MuiActionCardContainer/MuiActionCardContainer.test.js
+++ src/lib/components/MuiActionCardContainer/MuiActionCardContainer.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { mount } from 'enzyme';
 import { MemoryRouter } from 'react-router-dom';
 import renderer from 'react-test-renderer';
 import MuiActionCardContainer from './MuiActionCardContainer';
@@ -31,7 +31,7 @@ function setup() {
       }],
     }],
   };
-  const comp = shallow(<MuiActionCardContainer {...props} />);
+  const comp = mount(<MuiActionCardContainer {...props} />);
   return { comp, props };
 }
 
