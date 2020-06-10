e9801e32e606efe07a842c900322bf915a3adbe2
alex james <apjames93@gmail.com>
2020-06-10 11:51:36 -0600

    removed proptyps that are in Grid

--- src/lib/components/MuiHeroHeader/MuiHeroHeader.js
+++ src/lib/components/MuiHeroHeader/MuiHeroHeader.js
@@ -59,67 +59,14 @@ const MuiHeroHeader = (props) => {
 export default MuiHeroHeader;
 
 MuiHeroHeader.propTypes = {
-  /**
-   * stroyblok multiselect of css classes
-   * Mui Override or extend the styles applied to the component.
-   See CSS API below for more details.
-   */
-  rootClass: PropTypes.arrayOf(PropTypes.string),
-
   /** url for background img */
   backgroundImageUrl: PropTypes.string,
 
   /** height of the container */
   height: PropTypes.string,
-  /**
-   * mui prop: 'stretch'| 'center'| 'flex-start'| 'flex-end'| 'space-between'| 'space-around'
-   * Defines the align-content style property. It's applied for all screen sizes.
-   */
-  alignContent: PropTypes.string,
-  /**
-   * mui prop: 'flex-start' | 'center' | 'flex-end' | 'stretch' | 'baseline'
-   * Defines the align-items style property. It's applied for all screen sizes.
-   */
-  alignItems: PropTypes.string,
-  /*
-  * mui prop: 'row' | 'row-reverse' | 'column' | 'column-reverse'
-  * Defines the flex-direction style property. It is applied for all screen sizes.
-  */
-  direction: PropTypes.string,
-  /**
-   * mui prop:
-   'flex-start' | 'center' | 'flex-end' | 'space-between' | 'space-around' | 'space-evenly'
-   * Defines the justify-content style property. It is applied for all screen sizes.
-   */
-  justify: PropTypes.string,
-  /**
-   * mui prop:  0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10
-   * Defines the space between the type item component.
-   It can only be used on a type container component.
-   */
-  spacing: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
-  /**
-   * mui prop: 'nowrap' | 'wrap' | 'wrap-reverse'
-   * Defines the flex-wrap style property. It's applied for all screen sizes.
-   */
-  wrap: PropTypes.string,
-  /**
-   * Content passed to render
-   * components: MuiGrid
-   */
-  content: PropTypes.arrayOf(PropTypes.shape({
-    component: PropTypes.string.isRequired,
-  })).isRequired,
 };
 
 MuiHeroHeader.defaultProps = {
   height: '100%',
-  alignContent: 'stretch',
-  alignItems: 'center',
-  rootClass: [],
-  direction: 'row',
-  justify: 'center',
-  spacing: '2',
-  wrap: 'wrap',
   backgroundImageUrl: '',
 };
