f693c74509d7517e8b6af7ddabcd0fca01580953
alex james <apjames93@gmail.com>
2020-06-11 15:38:17 -0600

    button and iconbutton will now take a tooltip and if it is there wrap the btn with the tip

--- src/lib/components/MuiIconButton/MuiIconButton.test.js
+++ src/lib/components/MuiIconButton/MuiIconButton.test.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import { mount } from 'enzyme';
 import { MemoryRouter } from 'react-router-dom';
+import ReactDOM from 'react-dom';
+import { act } from 'react-dom/test-utils';
 import renderer from 'react-test-renderer';
 import Storyblok from '../../utils/Storyblok';
 import MuiIconButton from './MuiIconButton';
 
 Storyblok.arrayToMuiStyles = jest.fn(() => ({}));
 
-function setup() {
+function setup(tooltip = []) {
   const props = {
+    tooltip,
     icon: [{
       component: 'MuiIcon',
       iconName: 'star',
@@ -23,6 +26,47 @@ describe('<MuiIconButton />', () => {
   it('renders MuiIconButton', () => {
     const { comp } = setup();
     expect(comp).toBeDefined();
+  });
+
+  describe('tooltip', () => {
+    let container;
+
+    beforeEach(() => {
+      container = document.createElement('div');
+      document.body.appendChild(container);
+    });
+
+    afterEach(() => {
+      document.body.removeChild(container);
+      container = null;
+    });
+
+
+    it('shows tooltip', () => {
+      const tooltip = [{
+        title: 'cool',
+      }];
+      const { props } = setup(tooltip);
+
+      const tip = container.querySelector("[data-testid='tooltip']");
+
+      act(() => {
+        ReactDOM.render(<MuiIconButton {...props} />, container);
+      });
+
+      expect(tip).toBeDefined();
+    });
+
+    it('hides tooltip', () => {
+      const { props } = setup();
+
+      const tip = container.querySelector("[data-testid='tooltip']");
+
+      act(() => {
+        ReactDOM.render(<MuiIconButton {...props} />, container);
+      });
+      expect(tip).toBe(null);
+    });
   });
 
   describe('click', () => {
