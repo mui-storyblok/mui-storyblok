ed56b1fc665467383acfceee57950c1c6e010a09
Isaac Sunoo <isaacsunoo@gmail.com>
2020-06-12 15:28:23 -0600

    started adding tests for MuiButtonDialog but found a bug

--- src/lib/components/MuiButtonDialog/MuiButtonDialog.test.js
+++ src/lib/components/MuiButtonDialog/MuiButtonDialog.test.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { act } from 'react-dom/test-utils';
 import { mount } from 'enzyme';
 import { MemoryRouter } from 'react-router-dom';
 import renderer from 'react-test-renderer';
@@ -59,5 +60,16 @@ describe('<MuiButtonDialog />', () => {
       </MemoryRouter>
     ));
     expect(tree).toMatchSnapshot();
+  });
+
+  it('should toggle dialog onClick in the button component', () => {
+    const { comp } = setup();
+    console.log(comp.debug());
+    const initialOpen = comp.find('WithStyles(ForwardRef(Dialog))').first().prop('open');
+    expect(initialOpen).toEqual(false);
+    const firstButton = comp.find('.MuiButtonBase-root').first().prop('onClick');
+    act(() => {
+      firstButton();
+    });
   });
 });
