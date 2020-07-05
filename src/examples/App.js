34cc5714df4cdd85e94f957bd70022ea4b50a356
alex james <apjames93@gmail.com>
2020-07-05 12:35:48 -0600

    pass custom comps to example

--- src/examples/App.js
+++ src/examples/App.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import MuiStoryblok from '../lib/index';
 
+const MyCoolComp = () => <span>what?!</span>
+
 const App = () => (
   <MuiStoryblok
-    useObjectTheme={false}
+    useObjectTheme={true}
+    theme={{
+      palette: {
+        primary: {
+          main: '#42B540',
+        },
+      },
+    }}
     accessToken="9K1dqG8gQkc1jeVbOWRDUAtt"
     version="draft"
+    customComponents={[{ componentName: 'MyCoolComp', Component: MyCoolComp, props: {} }]}
   />
 );
 
