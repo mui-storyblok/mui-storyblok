df37fc54ff709a4d27b7d0f5522b8af5062d19ff
alex james <apjames93@gmail.com>
2020-07-05 19:26:43 -0600

    cc fix

--- src/lib/components/MuiList/components/MuiListItem/components/MuiListItemSecondaryAction/MuiListItemSecondaryAction.js
+++ src/lib/components/MuiList/components/MuiListItem/components/MuiListItemSecondaryAction/MuiListItemSecondaryAction.js
@@ -1,4 +1,4 @@
-import React, { createElement } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
 import Storyblok from '../../../../../../utils/Storyblok';
