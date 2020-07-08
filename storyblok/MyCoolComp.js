1fbfb426eb62f79093e2709ad0a8525913005d97
Isaac Sunoo <isaacsunoo@gmail.com>
2020-07-08 14:54:13 -0600

    incremented version and exported storyblok components

---
+++ storyblok/MyCoolComp.js
@@ -0,0 +1,15 @@
+module.exports = {
+  "name": "MyCoolComp",
+  "display_name": null,
+  "updated_at": "2020-07-05T18:13:35.050Z",
+  "schema": {},
+  "image": null,
+  "preview_field": null,
+  "is_root": false,
+  "preview_tmpl": null,
+  "is_nestable": true,
+  "all_presets": [],
+  "preset_id": null,
+  "real_name": "MyCoolComp",
+  "component_group_uuid": null
+}
