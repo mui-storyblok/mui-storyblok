ffde56d45b66d2ced2ce3212436e06b56720140b
Isaac Sunoo <isaacsunoo@gmail.com>
2020-07-09 14:09:21 -0600

    added 100% testing to tilebar-content

--- src/lib/components/MuiGridList/components/MuiGridListTileBar/MuiGridListTileBar.test.js
+++ src/lib/components/MuiGridList/components/MuiGridListTileBar/MuiGridListTileBar.test.js
@@ -3,28 +3,12 @@ import { shallow } from 'enzyme';
 import renderer from 'react-test-renderer';
 import MuiGridListTileBar from './MuiGridListTileBar';
 
-function setup() {
+function setup(actionComponent = 'MuiIconButtonRedirect') {
   const props = {
     titlePosition: 'bottom',
     subtitle: 'wooooo',
     title: 'ayeeeee',
-    actionIcon: [
-      {
-        component: 'MuiIconButtonRedirect',
-        iconButton: [
-          {
-            component: 'MuiIconButton',
-            icon: [
-              {
-                component: 'MuiIcon',
-                iconName: 'home',
-              },
-            ],
-          },
-        ],
-        redirectRoute: 'home',
-      },
-    ],
+    actionIcon: [{ component: actionComponent }],
   };
 
   const comp = shallow(<MuiGridListTileBar {...props} />);
@@ -41,5 +25,47 @@ describe('<MuiGridListTileBar />', () => {
   it('should render MuiGridListTileBar', () => {
     const { comp } = setup();
     expect(comp).toBeDefined();
+  });
+
+  it('should render MuiIconButton as actionItem in grid list tile bar', () => {
+    const { comp } = setup('MuiIconButton');
+    const actionIcon = comp.find('WithStyles(ForwardRef(GridListTileBar))').prop('actionIcon').props;
+    expect(actionIcon.component).toEqual('MuiIconButton');
+  });
+
+  it('should render MuiIconButtonDialog as actionItem in grid list tile bar', () => {
+    const { comp } = setup('MuiIconButtonDialog');
+    const actionIcon = comp.find('WithStyles(ForwardRef(GridListTileBar))').prop('actionIcon').props;
+    expect(actionIcon.component).toEqual('MuiIconButtonDialog');
+  });
+
+  it('should render MuiIconButtonHref as actionItem in grid list tile bar', () => {
+    const { comp } = setup('MuiIconButtonHref');
+    const actionIcon = comp.find('WithStyles(ForwardRef(GridListTileBar))').prop('actionIcon').props;
+    expect(actionIcon.component).toEqual('MuiIconButtonHref');
+  });
+
+  it('should render MuiIconButtonRedirect as actionItem in grid list tile bar', () => {
+    const { comp } = setup('MuiIconButtonRedirect');
+    const actionIcon = comp.find('WithStyles(ForwardRef(GridListTileBar))').prop('actionIcon').props;
+    expect(actionIcon.component).toEqual('MuiIconButtonRedirect');
+  });
+
+  it('should return null as actionItem in grid list tile bar', () => {
+    const { comp } = setup('Apples');
+    const actionIcon = comp.find('WithStyles(ForwardRef(GridListTileBar))').prop('actionIcon');
+    expect(actionIcon).toEqual(null);
+  });
+
+  it('should return renderAction as null if actionIcon is empty', () => {
+    const props = {
+      titlePosition: 'bottom',
+      subtitle: 'wooooo',
+      title: 'ayeeeee',
+      actionIcon: [],
+    };
+    const comp = shallow(<MuiGridListTileBar {...props} />);
+    const actionIcon = comp.find('WithStyles(ForwardRef(GridListTileBar))').prop('actionIcon');
+    expect(actionIcon).toEqual(null);
   });
 });
