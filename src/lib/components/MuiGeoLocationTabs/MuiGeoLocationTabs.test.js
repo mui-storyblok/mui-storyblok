5232cbda82d45cb7c7e260770f363ec83d3ef960
Isaac Sunoo <isaacsunoo@gmail.com>
2020-08-13 11:49:49 -0600

    fixed cc issue with geo location tabs testing setup line exceded

--- src/lib/components/MuiGeoLocationTabs/MuiGeoLocationTabs.test.js
+++ src/lib/components/MuiGeoLocationTabs/MuiGeoLocationTabs.test.js
@@ -3,42 +3,27 @@ import React from 'react';
 import { mount } from 'enzyme';
 import MuiGeoLocationTabs from './MuiGeoLocationTabs';
 
-function setup() {
-  const props = {
-    tabs: [{
-      component: 'MuiTab',
-      label: 'First Tab option',
+const tab = [{
+  component: 'MuiTab',
+  label: 'First Tab option',
+  content: [{
+    component: 'MuiGrid',
+    content: [{
+      component: 'MuiGridItem',
       content: [{
-        component: 'MuiGrid',
+        component: 'MuiTypography',
         content: [{
-          component: 'MuiGridItem',
-          content: [{
-            component: 'MuiTypography',
-            content: [{
-              component: 'MuiText',
-              text: 'First tab text',
-            }],
-          }],
-        }],
-      }],
-    },
-    {
-      component: 'MuiTab',
-      label: 'Second Tab option',
-      content: [{
-        component: 'MuiGrid',
-        content: [{
-          component: 'MuiGridItem',
-          content: [{
-            component: 'MuiTypography',
-            content: [{
-              component: 'MuiText',
-              text: 'Second tab text',
-            }],
-          }],
+          component: 'MuiText',
+          text: 'First tab text',
         }],
       }],
     }],
+  }],
+}];
+
+function setup() {
+  const props = {
+    tabs: [...tab, ...tab],
   };
   const comp = mount(<MuiGeoLocationTabs {...props} />);
   return { comp, props };
