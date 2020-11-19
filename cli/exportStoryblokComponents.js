0e417c1ccf08deae5d31a91b5f14b023d47feec2
alex james <apjames93@gmail.com>
2020-11-18 20:17:34 -0700

    clean up script

--- cli/exportStoryblokComponents.js
+++ cli/exportStoryblokComponents.js
@@ -3,9 +3,6 @@
 require('dotenv').config();
 const { exec } = require('child_process');
 
-const cmd = `storyblok pull-components --space ${process.env.STORYBLOK_SPACE_ID}`;
-const moveStoryblokComponents = `mv ./components.${process.env.STORYBLOK_SPACE_ID}.json ./storyblok/components.json `;
-
 const asyncCmd = (command) => {
   console.log(command);
   return new Promise((resolve, reject) => {
@@ -21,9 +18,11 @@ const asyncCmd = (command) => {
   });
 };
 
-const migrateComponents = async (command) => {
-  await asyncCmd(command);
+const migrateComponents = async () => {
+  const cmd = `storyblok pull-components --space ${process.env.STORYBLOK_SPACE_ID}`;
+  const moveStoryblokComponents = `mv ./components.${process.env.STORYBLOK_SPACE_ID}.json ./storyblok/components.json `;
+  await asyncCmd(cmd);
   await asyncCmd(moveStoryblokComponents);
 };
 
-migrateComponents(cmd);
+migrateComponents();
