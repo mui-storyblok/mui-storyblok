2471ac8e2ef0746ebabaef5005fee02126e7a28a
alex james <apjames93@gmail.com>
2020-11-24 21:07:33 -0700

    set up geolocation tabs

---
+++ src/lib/components/PageGrid/templates/GeoLocationTabs/GeoLocationTabs.js
@@ -0,0 +1,41 @@
+/* eslint-disable max-len */
+import React, { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
+import Tabs from 'lib/components/PageGrid/templates/Tabs/Tabs';
+import { validComponentsRequired } from 'lib/utils/customProps';
+import { useSetGeoCode } from 'lib/utils/geoLocate';
+
+const MuiGeoLocationTabs = ({
+  tabs,
+  googleApiKey,
+
+}) => {
+  const [googleKey, setGoogleApiKey] = useState(googleApiKey);
+  const [tabValue, setTabValue] = useState(0);
+
+  useEffect(() => {
+    setGoogleApiKey(googleApiKey);
+  }, [googleApiKey]);
+
+  useSetGeoCode(true, tabs, setTabValue, googleKey);
+
+  return (<Tabs {...tabs[0]} tabIndex={tabValue} />);
+};
+
+export default MuiGeoLocationTabs;
+
+MuiGeoLocationTabs.propTypes = {
+  /** key for googleApisKey to use geocode */
+  googleApiKey: PropTypes.string,
+
+  /** MuiTabs */
+  tabs(props, propName, componentName) {
+    const components = ['Tabs'];
+    return validComponentsRequired(props, propName, componentName, components, 1);
+  },
+};
+
+MuiGeoLocationTabs.defaultProps = {
+  tabs: [],
+  googleApiKey: '',
+};
